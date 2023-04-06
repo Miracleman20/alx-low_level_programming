a17d2f2e21d9dfd834d27f7bc421275156b7347c
Miracleman20 <vkta20@gmail.com>
2023-04-06 06:31:07 -0700

    string length in a recursive function

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - recursion function to return the length of a string
+ *@s: is the character string of the recursive function
+ *
+ *Return: returns 1 when program executes correctly
+ */
+
+int _strlen_recursion(char *s)
+{
+	int c = 0;
+
+	if (*s)
+	{
+		c++;
+		c += _strlen_recursion(*s + 1);
+	}
+	return (c);
+}
