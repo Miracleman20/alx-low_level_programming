e8fe8a289858e21776bb31b31bb5f346a9fab492
Miracleman20 <vkta20@gmail.com>
2023-04-06 08:09:06 -0700

    factorial files

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int factorial(int n)
+{
+	int fact = n;
+
+	if (n < 0)
+		return (-1);
+
+	else if (n >= 0 && n <= 1)
+		return (1);
+
+	fact *= factorial(n - 1);
+
+	return (fact);
+}
