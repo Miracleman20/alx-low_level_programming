466bd523983563cdaf144dc8eb08c9c7c746e9cd
Miracleman20 <vkta20@gmail.com>
2023-04-07 07:00:02 -0700

    adding ppalindrome task into commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,3 +17,44 @@ int is_palindrome(char *s)
 
 	return (check_palindrome(s, str, index));
 }
+
+/**
+ *check_palindrome - is a function that checks for if a string is a palindrome
+ *@s: is the string holding variable
+ *@str: is the string to be cjecked for being a palindrome
+ *@index: is the index of a charcter in a string pointer
+ *
+ * Return: returns a value of 1
+ */
+
+int check_palindrome(char *s, int str, int index)
+{
+	if (s[index] == s[str / 2])
+		return (1);
+
+	if (s[index] == s[str - index - 1])
+		return (check_palindrome(s, str, index + 1));
+
+	return (0);
+}
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int find_strlen(char *s)
+{
+	int str = 0;
+
+	if (*(s + str))
+	{
+		str++;
+		str += find_strlen(s + str);
+	}
+
+	return (str);
+}
