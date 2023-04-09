b06433f73a29ba3edacce13851d55d4959614b9c
Miracleman20 <vkta20@gmail.com>
2023-04-09 15:34:50 -0700

    error rectifictaion

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
-
+/**
+ *_puts - this enables output of strings and characters
+ *@str: this is a string literal parameter
+ *
+ * Return: returns a value after execution
+ */
 
 void _puts(char *str)
 {
 	int i;
 
-	for (str[i])
+	while (str[i])
 	{
 		_putchar(str[i]);
 		i++;
 	}
 
-	return ('\n');
+	_putchar('\n');
 }
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
  *_putchar - this function outputs character strings
