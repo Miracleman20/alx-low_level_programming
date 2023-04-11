2030521f1acc8e480c0129a07a9c7b2016dcb9ac
Miracleman20 <vkta20@gmail.com>
2023-04-11 16:15:07 -0700

    strtow sample

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *strtow - a function preprocessor for splitting a
+ *	string into two
+ *
+ * @str: is the string to be split
+ *
+ *Return: returns a positive value
+ */
+
+char **strtow(char *str)
+{
+	int i;
+	char *arr;
+
+	/**
+	 * arr = malloc(sizeof())
+	 */
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	if (arr == NULL)
+		return (NULL);
+}
